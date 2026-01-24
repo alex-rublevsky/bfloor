f3bd73a80f8bffb4afcdb04d1cda12d28c94511f
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-01-24 00:28:18 -0500

    updated prod url

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -5,7 +5,7 @@ import { createAuthClient } from "better-auth/react";
 const baseURL =
 	typeof window !== "undefined"
 		? window.location.origin
-		: process.env.BETTER_AUTH_URL || "https://bfloor.vercel.app/";
+		: process.env.BETTER_AUTH_URL || "https://bfloor.ru/";
 
 export const { useSession, signIn, signOut, getSession } = createAuthClient({
 	baseURL: baseURL,
