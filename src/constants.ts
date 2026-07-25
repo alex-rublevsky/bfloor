9e6ea5ad693c0d8cbd104a02b172c72cc5bf70ff
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-07-25 14:19:54 +1000

    updated Head and the doc title

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
 export const SITE_TITLE = "BeautyFloor";
-export const SITE_DESCRIPTION = "Напольные покрытия во Владивостоке";
+export const SITE_DESCRIPTION = "Напольные покрытия и укладка во Владивостоке";
+//TODO: написать
+export const SITE_META_DESCRIPTION = "";
 export const BASE_IMAGE_URL =
   "https://storage.yandexcloud.net/bfloor-static/images/";
+export const STAGING_PREFIX = "images/staging/";
