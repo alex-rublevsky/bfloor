4ed6bad70bea2fef8b142e5212f5fbcf23fdb5c5
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-07-31 14:35:03 +1000

    misc

--- src/db/dashboard/createProduct.ts
+++ src/db/dashboard/createProduct.ts
@@ -1,6 +1,6 @@
 import { db } from "@/db/index";
-import { categories, products } from "@/db/schema";
-import { eq, type InferSelectModel } from "drizzle-orm";
+import { products } from "@/db/schema";
+import { type InferSelectModel } from "drizzle-orm";
 
 export type Product = Pick<
   InferSelectModel<typeof products>,
