8da861aaff26c97ba8c9f4444215ef62f1d0136b
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-08-13 13:17:43 +1000

    created drafts of: dashboard category page, category edit page, category new page, category form, getCategoryBySlug

---
+++ src/db/dashboard/categoryBySlug.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db/index";
+import { categories } from "@/db/schema";
+import { eq, type InferSelectModel } from "drizzle-orm";
+
+export type Category = Pick<
+  InferSelectModel<typeof categories>,
+  | "id"
+  | "name"
+  | "image"
+  | "slug"
+>;
+
+export async function getCategoryBySlug(
+  slug?: string,
+): Promise<Category | null> {
+  if (!slug) throw new Error("slug is required");
+
+  const row = await db.query.categories.findFirst( { where: {
+        slug: slug
+      }});
+
+  return row ?? null;
+}
