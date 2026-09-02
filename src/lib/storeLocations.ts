d7810801576cbf8b2ca9db859d0bcabdf5cc7c88
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-09-02 10:41:40 +1000

    + new store location

--- src/lib/storeLocations.ts
+++ src/lib/storeLocations.ts
@@ -1,14 +1,20 @@
 export const STORE_LOCATIONS = [
   {
-    id: 1,
+    id: 0,
     address: "ул. Русская, 78",
     description: "Вход со стороны дороги",
     openingHours: "Пн — Пт: 10 — 18\nСб: 11 — 17\nВс: Выходной",
   },
   {
-    id: 2,
+    id: 1,
     address: "ул. 100 летия, 30",
     description: null,
     openingHours: "Пн — Пт: 10 — 18\nСб: 11 — 17\nВс: Выходной",
+  },
+  {
+    id: 2,
+    address: "ул. Бородинская 46/50",
+    description: `Торговый центр "Виктория", павильон №14`,
+    openingHours: "Пн — Пт: 9 — 18\nВс — Вс: 11 — 17",
   },
 ] as const;
